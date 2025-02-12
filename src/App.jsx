d370ca46730a731df54f90e7fcf005b26473fbbf
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-02-12 22:54:41 +0000

    add redirect from home to first category

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import Category from "./pages/Category";
 import Entry from "./pages/Entry";
+import Header from "./components/Header";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/creatures" replace />} />
-            <Route path="/:category" element={<Category />} />
-            <Route path="/entry/:id" element={<Entry />} />
-        </Routes>
+        <>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Navigate to="/creatures" replace />}
+                />
+                <Route path="/:category" element={<Category />} />
+                <Route path="/entry/:id" element={<Entry />} />
+            </Routes>
+        </>
     );
 }
 
