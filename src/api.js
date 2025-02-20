a8ad1cbc4e13a1d0694fd36dc64c435982569d81
Punit Shah <pshah@turnitin.com>
2025-02-20 15:09:48 +0000

    add swr for data fetching and refactor useApiRequest to utilize it

--- src/api.js
+++ src/api.js
@@ -1,28 +1,15 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const api = axios.create({
     baseURL: "https://botw-compendium.herokuapp.com/api/v3/compendium",
 });
 
-export const useApiRequest = (path, { method = "get", params = {} } = {}) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+const fetcher = ({ path, method, params }) =>
+    api.request(path, { method, params }).then(({ data }) => data.data);
 
-    useEffect(() => {
-        api.request(path, { method, params })
-            .then(({ data }) => {
-                setData(data.data);
-            })
-            .catch((error) => {
-                setError(error);
-            });
-    }, [path, method, params]);
-
-    const isLoading = data === null && error === null;
-
-    return { data, error, isLoading };
-};
+export const useApiRequest = (path, { method = "get", params = {} } = {}) =>
+    useSWR({ path, method, params }, fetcher);
 
 export const useCategory = (category) => {
     const { data, error, isLoading } = useApiRequest(`/category/${category}`);
